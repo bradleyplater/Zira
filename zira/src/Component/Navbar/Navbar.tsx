486bfe037ffdc094c2883d707515b5f3f80b9501
Bradley <Bradley.plater1@gmail.com>
2021-01-19 08:36:57 +0000

    Created Skeleton Nav component

--- zira/src/Component/Navbar/Navbar.tsx
+++ zira/src/Component/Navbar/Navbar.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../Navbar/Navbar.css';
 
 interface Props {
     navItems: string[];
 }
 
-export function Navbar({ navItems }: Props): React.ReactNode {
-    return <div></div>;
+export default function Navbar({ navItems }: Props): JSX.Element {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    Zira
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <ul className="navbar-nav">
+                        {navItems.map((navItem) => {
+                            //CHECK THE BOOTSTAP DOCKS TO GET THE CORRECT NAV SHIT
+
+                            <li className="nav-item dropdown">
+                                <NavLink
+                                    key={navItem}
+                                    to={'/' + navItem}
+                                    className="nav-link"
+                                    activeClassName="nav-link active"
+                                >
+                                    {navItem}
+                                </NavLink>
+                            </li>;
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 }
