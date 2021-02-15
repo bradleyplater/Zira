deaca85191bf939fdda83cb2b0042f743e8be2fe
Bradley Plater <bradley.plater@bglgroup.co.uk>
2021-02-15 14:54:07 +0000

    Redux related tests are working

--- zira/src/Component/Profile/__tests__/Profile.test.tsx
+++ zira/src/Component/Profile/__tests__/Profile.test.tsx
@@ -3,20 +3,25 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { applyMiddleware, CombinedState, combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { fireEvent, render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import Profile from '../Profile';
 import { User } from '../../../State/Models/UserModels';
 import thunk from 'redux-thunk';
 import { AuthProps } from '../../../Models/PropTypes';
-import nock from 'nock';
 import userReducer from '../../../State/User/userReducer';
-import evenutally from 'mocha-eventually';
+import mockedAxios from '../../../__mocks__/axios';
+import TestComponent from '../../TestHelpers/TestComponent';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function renderWithRedux(component: JSX.Element) {
     const store = setUpStore();
-    return render(<Provider store={store}>{component}</Provider>);
+    return render(
+        <Provider store={store}>
+            <Router>{component}</Router>
+        </Provider>,
+    );
 }
 const email = 'test@email.com';
 const auth: AuthProps = { isAuthenticated: true, user: { name: 'test', email: email } };
@@ -25,23 +30,25 @@ function setUpStore() {
 }
 
 describe('Redux Tests', () => {
-    afterAll(() => nock.cleanAll());
-
+    jest.mock('react-router-dom', () => {
+        useHistory: () => ({
+            push: jest.fn(),
+        });
+    });
     it('Renders with Redux', () => {
+        const user: User = { name: 'test', email: email };
+        const data = { data: user, status: 200 };
+        mockedAxios.get.mockResolvedValueOnce(data);
         renderWithRedux(<Profile auth={auth}></Profile>);
     });
 
-    it('When user does not exist navigate to /create-profile', () => {
-        const { getByText } = renderWithRedux(<Profile auth={auth}></Profile>);
+    it('When user does not exist navigate to /create-profile', async () => {
         const user: User = { name: 'test', email: email };
+        const data = { data: user, status: 200 };
+        mockedAxios.get.mockResolvedValueOnce(data);
+        const { getByText } = renderWithRedux(<Profile auth={auth}></Profile>);
 
-        nock('https://localhost:44353')
-            .defaultReplyHeaders({ 'access-control-allow-origin': '*', 'access-control-allow-credentials': 'true' })
-            .persist()
-            .get(`/api/v1/users?email=${email}`)
-            .reply(200, user);
-
-        return evenutally(() => {
+        await waitFor(() => {
             getByText('Name - test');
         });
     });
