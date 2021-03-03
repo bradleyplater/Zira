ab877b1487805385d454ea4ca9406fb7940073f4
Bradley <Bradley.plater1@gmail.com>
2021-03-03 22:25:12 +0000

    Added new view state to existing components

--- zira/src/Component/CreateProfile/CreateProfile.tsx
+++ zira/src/Component/CreateProfile/CreateProfile.tsx
@@ -1,10 +1,22 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { View } from '../../State/Models/ViewsModels';
+import { RootStore } from '../../State/Store';
+import { SetCurrentView } from '../../State/Views/ViewsActions/ViewsActions';
 import CreateProfileForm from '../CreateProfileForm/CreateProfileForm';
 import './CreateProfile.css';
 
 export default function CreateProfile() {
     const { isAuthenticated, user } = useAuth0();
+
+    const viewsState = useSelector((state: RootStore) => state.views);
+    const dispatch = useDispatch();
+
+    if (viewsState.currentView != View.CreateProfile) {
+        dispatch(SetCurrentView(View.CreateProfile));
+    }
+
     return (
         <div className="background-container">
             <div className="form-container">
--- zira/src/Component/LandingPage/LandingPage.tsx
+++ zira/src/Component/LandingPage/LandingPage.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { View } from '../../State/Models/ViewsModels';
+import { RootStore } from '../../State/Store';
+import { SetCurrentView } from '../../State/Views/ViewsActions/ViewsActions';
 import './LandingPage.css';
 
 export default function LandingPage() {
+    const viewsState = useSelector((state: RootStore) => state.views);
+    const dispatch = useDispatch();
+
+    if (viewsState.currentView != View.LandingPage) {
+        dispatch(SetCurrentView(View.LandingPage));
+    }
     return (
         <div id="intro" className="view">
             <div className="mask"></div>
