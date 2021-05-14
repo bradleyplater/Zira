fa27c0ba0f47e1f0e59eb489d4ce615117859fc3
Bradley <Bradley.plater1@gmail.com>
2021-05-14 23:15:14 +0100

    Added User properties

--- ziraApi/Zira.Core/Models/User.cs
+++ ziraApi/Zira.Core/Models/User.cs
@@ -6,6 +6,11 @@ namespace Zira.Core.Models
 {
     class User
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
 
+        public List<Issue> Issues { get; set; }
     }
 }
