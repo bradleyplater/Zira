4c21938a1070ad980f41892207490dfaf9027781
Bradley <Bradley.plater1@gmail.com>
2021-05-15 09:34:06 +0100

    Inherited from base user interface and removed issues list

--- ziraApi/Zira.Core/Models/User.cs
+++ ziraApi/Zira.Core/Models/User.cs
@@ -1,16 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Zira.Core.Interfaces;
 
 namespace Zira.Core.Models
 {
-    class User
+    class User : IBaseUser
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string Surname { get; set; }
         public string Email { get; set; }
 
-        public List<Issue> Issues { get; set; }
     }
 }
