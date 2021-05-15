6d6c1a6797dbc3ba95b47ab5f68bdcb396029072
Bradley <Bradley.plater1@gmail.com>
2021-05-15 14:53:55 +0100

    Created User DTO

---
+++ ziraApi/Zira.Core/Models/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zira.Core.Models
+{
+    class UserDto
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
