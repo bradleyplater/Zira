b7e4e763ff41400191f4140af4d91d2c243defe9
Bradley <Bradley.plater1@gmail.com>
2021-05-15 11:54:22 +0100

    Created BsonCollection attribute

---
+++ ziraApi/Zira.Core/Attributes/BsonCollectionAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zira.Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    class BsonCollectionAttribute : Attribute
+    {
+        public string CollectionName { get; }
+        public BsonCollectionAttribute(string collectionName)
+        {
+            CollectionName = collectionName;
+        }
+    }
+}
