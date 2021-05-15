a84857e02a3482ec47b8d8c68227cf74ddcd7d3c
Bradley <Bradley.plater1@gmail.com>
2021-05-15 12:24:20 +0100

    Added get by email for users

---
+++ ziraApi/Zira.Api/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zira.Core.Models;
+using Zira.Services.Services;
+
+namespace Zira.Api.Controllers
+{
+    [Route("api/v1/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<User>> GetUserByEmail(string email)
+        {
+            var user = _userService.GetUserByEmailAsync(email);
+            if(user.Result != null)
+            {
+                return Ok(await user);
+            } else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
