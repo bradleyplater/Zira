0d165059721e2aa38596cbc62787abce4792716b
Bradley <Bradley.plater1@gmail.com>
2021-05-15 11:56:18 +0100

    removed default class from data

---
+++ ziraApi/Zira.Data/IMongoDbSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zira.Data
+{
+    public interface IMongoDbSettings
+    {
+        string DatabaseName { get; set; }
+        string ConnectionString { get; set; }
+    }
+}
