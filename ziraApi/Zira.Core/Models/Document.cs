66eb54480f1a84f57bc89b7e1451d46bdc123448
Bradley <Bradley.plater1@gmail.com>
2021-05-15 11:54:41 +0100

    Created Base Document types

---
+++ ziraApi/Zira.Core/Models/Document.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zira.Core.Models
+{
+    public abstract class Document : IDocument
+    {
+        public ObjectId ObjectId { get; set; }
+        public DateTime CreatedAt => ObjectId.CreationTime;
+    }
+}
---
+++ ziraApi/Zira.Core/Models/IDocument.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zira.Core.Models
+{
+    public interface IDocument
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        ObjectId ObjectId { get; set; }
+
+        DateTime CreatedAt { get; }
+    }
+}
