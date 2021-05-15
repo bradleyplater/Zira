9430fe37dd4d0e34eb445894d1508354c1bbe292
Bradley <Bradley.plater1@gmail.com>
2021-05-15 11:53:29 +0100

    Configured mongo services and set up repositories

--- ziraApi/Zira.Api/Startup.cs
+++ ziraApi/Zira.Api/Startup.cs
@@ -6,11 +6,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Zira.Core.Repositories;
+using Zira.Data;
 
 namespace Zira.Api
 {
@@ -26,6 +29,12 @@ namespace Zira.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.Configure<MongoDbSettings>(Configuration.GetSection("MongoDbSettings"));
+
+            services.AddSingleton<IMongoDbSettings>(serviceProvider => serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
+
+            services.AddScoped(typeof(IUserRepository<>), typeof(UserRepository<>));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
