4027ed7e78cedd084310c6c91e3166166f8798d3
Bradley <Bradley.plater1@gmail.com>
2021-05-15 09:33:51 +0100

    Created Base User Interface

---
+++ ziraApi/Zira.Core/Interfaces/IBaseUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zira.Core.Interfaces
+{
+    public interface IBaseUser
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string Surname { get; set; }
+        string Email { get; set; }
+    }
+}
