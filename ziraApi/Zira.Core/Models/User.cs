9bb517f70c1fbb45330b8d64b4de71d70796182e
Bradley <Bradley.plater1@gmail.com>
2021-05-15 09:49:59 +0100

    Removed unused using statements

--- ziraApi/Zira.Core/Models/User.cs
+++ ziraApi/Zira.Core/Models/User.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Zira.Core.Interfaces;
-
-namespace Zira.Core.Models
+﻿namespace Zira.Core.Models
 {
     class User : IBaseUser
     {
