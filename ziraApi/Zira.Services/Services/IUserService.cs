dd44254684a3c9e07f8033127df3d7e03b02dd18
Bradley <Bradley.plater1@gmail.com>
2021-05-15 09:54:17 +0100

    Created IUserService

---
+++ ziraApi/Zira.Services/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Zira.Core.Models;
+
+namespace Zira.Services.Services
+{
+    public interface IUserService
+    {
+        Task<IBaseUser> GetUserByEmail(string email);
+    }
+}
