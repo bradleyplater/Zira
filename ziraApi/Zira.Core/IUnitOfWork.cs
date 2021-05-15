bfd1eeff116185d54a95524025be999517491efd
Bradley <Bradley.plater1@gmail.com>
2021-05-15 09:49:25 +0100

    Created unit of work interface

---
+++ ziraApi/Zira.Core/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Zira.Core.Repositories;
+
+namespace Zira.Core
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        Task<int> CommitAsync();
+    }
+}
